0f55b4ccae5cf44c9c1a7a96909ae937c578b336
Elise Beall <elisebeall@gmail.com>
2021-12-05 15:08:11 -0700

    Change structure of movie detail elements

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -20,37 +20,28 @@ const MovieDetails = ({ posters, id }) => {
   } = movie
 
   return (
-    <section>
-      <header>
-        <h1>{title}</h1>
-        <div>
-          <h2>{release_date}</h2>
-          <h3>{runtime}</h3>
-        </div>
-        <div>
-          {/* <img /> */}
-          <p>⭐️</p>
-          <h3>Rating</h3>
-          <h3>{average_rating}</h3>
-        </div>
-        <div>
-          {/* <img /> */}
-          <p>$</p>
-          <h3>Budget</h3>
-          <h3>{budget}</h3>
-        </div>
-        <div>
-          {/* <img /> */}
-          <p>💲</p>
-          <h3>Gross</h3>
-          <h3>{revenue}</h3>
-        </div>
+    <section className="movie-details">
+      <header className="movie-details-header">
+        <h1 className="title">{title}</h1>
+        <h2 className="release">{release_date.split('-')[0]}</h2>
+        <h3 className="runtime">{runtime}</h3>
+        <p className="star">⭐️</p>
+        <h3 className="rating-label">Rating</h3>
+        <h3 className="rating">{average_rating.toFixed(1)}</h3>
+        <p className="dollar-sign1">$</p>
+        <h3 className="budget-label">Budget</h3>
+        <h3 className="budget">{budget}</h3>
+        <p className="dollar-sign2">💲</p>
+        <h3 className="gross-label">Gross</h3>
+        <h3 className="gross">{revenue}</h3>
       </header>
-      <h3>{genres}</h3>
-      <h3>{tagline}</h3>
-      <p>{overview}</p>
-      <img src={poster_path} alt={title}/>
-      <img />
+      <h3 className="genres">{genres}</h3>
+      <h3 className="tagline">{tagline}</h3>
+      <p className="overview">{overview}</p>
+      <div className="visuals">
+        <img className="poster" src={poster_path} alt={title}/>
+        <img className="trailer" src="https://i.stack.imgur.com/PtbGQ.png" alt="trailer placeholder"/>
+      </div>
     </section>
   )
 }
