7a9a654ebb9ccf89079574c6aa99ac4d476c205c
Elise Beall <elisebeall@gmail.com>
2021-12-04 21:33:02 -0700

    Added alt tag defined as props.title

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,7 +1,7 @@
 import '../css/Poster.css'
 
 const Poster = (props) => {
-    return <img src={props.image} alt='movie poster' onClick={() => props.onClick(props.id)}/>
+    return <img src={props.image} alt={props.title} onClick={() => props.onClick(props.id)}/>
 }
 
 export default Poster
