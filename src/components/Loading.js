6bb979f0848d130d4b2a6b8216120c5e3f83de91
Elise Beall <elisebeall@gmail.com>
2021-12-12 12:57:03 -0700

    Add Error & Loading components

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import '../css/Loading.css'
+import loadingIcon from '../assets/load2.gif'
+
+const Loading = () => {
+  return (
+    <img className="loading-icon" src={loadingIcon} alt="loading" />
+  )
+}
+
+export default Loading
